f196ae3914c8a9c5d2ae09ad534e0a182000893d
Guilherme dAlmeida <guilhermebarros181@gmail.com>
2025-12-11 23:30:34 -0300

    fix: cleaning up comments

--- Program.cs
+++ Program.cs
@@ -8,14 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<VeiculoContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 // app.UseHttpsRedirection();
