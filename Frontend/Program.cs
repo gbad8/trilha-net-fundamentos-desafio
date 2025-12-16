fd7d67bbb169de0892b5b128b8d7ba5274c2520c
Guilherme dAlmeida <guilhermebarros181@gmail.com>
2025-12-16 10:45:57 -0300

    feat: adding compose file

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://0:8000") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://0.0.0.0:8000") });
 
 await builder.Build().RunAsync();
