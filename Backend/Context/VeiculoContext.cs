aea9df9d8f22dfbcc813cdc025e3beb389b5627d
Guilherme dAlmeida <guilhermebarros181@gmail.com>
2025-12-26 14:11:21 -0300

    fix: adding precision to the TicketPrice column on SqlServer

--- Backend/Context/VeiculoContext.cs
+++ Backend/Context/VeiculoContext.cs
@@ -11,4 +11,13 @@ public class VeiculoContext : DbContext
   }
 
   public DbSet<Veiculo> Veiculos { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+
+    modelBuilder.Entity<Veiculo>()
+      .Property(v => v.TicketPrice)
+      .HasColumnType("decimal(18,2)");
+  }
 }
---
+++ Backend/Migrations/20251226165416_AdjustedDecimalPrecision.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace trilha_net_fundamentos_desafio.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustedDecimalPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
