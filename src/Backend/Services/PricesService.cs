0dc3edde52625282c588d9ef2021e3fa12422239
Guilherme dAlmeida <guilhermebarros181@gmail.com>
2026-01-19 10:25:46 -0300

    fix: simplifying redundancy in the PATCH service logic

--- src/Backend/Services/PricesService.cs
+++ src/Backend/Services/PricesService.cs
@@ -6,23 +6,20 @@ namespace trilha_net_fundamentos_desafio.Services;
 
 public class PricesService(VeiculoContext context) : IPricesService
 {
-    private readonly VeiculoContext _context = context;
+  private readonly VeiculoContext _context = context;
 
-    public async Task<IEnumerable<Prices>> GetAllPricesAsync()
-    {
-        return await _context.Prices.ToListAsync();
-    }
+  public async Task<IEnumerable<Prices>> GetAllPricesAsync()
+  {
+    return await _context.Prices.ToListAsync();
+  }
 
-    public async Task UpdatePricesAsync(List<Prices> updatedPrices)
+  public async Task UpdatePricesAsync(List<Prices> updatedPrices)
+  {
+    foreach (var updatedPrice in updatedPrices)
     {
-        foreach (var updatedPrice in updatedPrices)
-        {
-            var existingPrice = await _context.Prices.FindAsync(updatedPrice.Type);
-            if (existingPrice != null)
-            {
-                existingPrice.HourlyPrice = updatedPrice.HourlyPrice;
-            }
-        }
-        await _context.SaveChangesAsync();
+      var existingPrice = await _context.Prices.FindAsync(updatedPrice.Type);
+      existingPrice?.HourlyPrice = updatedPrice.HourlyPrice;
     }
+    await _context.SaveChangesAsync();
+  }
 }
