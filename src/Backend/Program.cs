f30c7cb5f3bb72e4bc246f7bf9a3998b56cac824
Guilherme d'Almeida <guilhermebarros181@gmail.com>
2026-01-20 11:32:36 -0300

    feat: new feature to create the database when the aplication run for the first time

--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -52,6 +52,24 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<VeiculoContext>();
+        if (context.Database.GetPendingMigrations().Any())
+        {
+            context.Database.Migrate();
+        }
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocorreu um erro ao migrar o banco de dados.");
+    }
+}
+
 app.UseStaticFiles();
 
 if (app.Environment.IsDevelopment())
